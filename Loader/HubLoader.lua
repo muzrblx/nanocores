ed5c3947830d8e5e0be69c3b8b6ba8eab5216c93
Owner Of Nano <87079094+muzrblx@users.noreply.github.com>
2022-04-13 16:54:22 -0700

    Update HubLoader.lua

--- Loader/HubLoader.lua
+++ Loader/HubLoader.lua
@@ -2,7 +2,7 @@ local dsinv = "https://discord.gg/EGwQdWPG3y"
 
 local d
 local f = pcall(function()
-    d = game:HttpGet("https://raw.githubusercontent.com/muzrblx/nanocore/main/NanoLoader/Scripts/"..game.PlaceId..".lua")
+    d = game:HttpGet("https://raw.githubusercontent.com/muzrblx/muz-scripts/main/NanoLoader/Scripts/"..game.PlaceId..".lua")
 end)
 if f == true then
     loadstring(d)()
